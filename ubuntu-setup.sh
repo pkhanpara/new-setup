11eeea17d67f2623df03ad4944e3df9ecddb8745
poojankhanpara <poojankhanpara@gmail.com>
2022-07-22 15:50:44 -0700

    removed third party ppas

--- ubuntu-setup.sh
+++ ubuntu-setup.sh
@@ -1,20 +1,5 @@
 #!/bin/sh
 
-# This is a simple script to set up ubuntu the way I like it.
-
-# add ppas
-echo 'Adding PPAs'
-#add-apt-repository ppa:webupd8team/sublime-text-3
-echo 'Done.'
-
-# adding keys
-echo 'adding keys to repo for spotify'
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys BBEBDCB318AD50EC6865090613B00F1FD2C19886
-echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
-
-echo 'adding keys for sublime text'
-wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
-
 # update & upgrade
 echo 'Performing system update'
 sudo apt-get update -y
